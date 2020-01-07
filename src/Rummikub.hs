a4e84b50a87acdd6476a066d4df3c4ed9fde9944
jfjhh <alex.striff1@gmail.com>
2020-01-07 00:46:34 -0500

    Tests for comparison

--- src/Rummikub.hs
+++ src/Rummikub.hs
@@ -3,6 +3,9 @@
 
 module Rummikub where
 
+import Prelude hiding
+  (Enum, succ, pred, toEnum, fromEnum, enumFrom, enumFromThen, enumFromTo, enumFromThenTo)
+import Prelude.SafeEnum
 import Control.Applicative
 import Control.Monad
 import Data.Function
@@ -15,6 +18,38 @@ import Text.Read
 infixr 9 <.>
 (<.>) :: Functor f => (a -> b) -> (c -> f a) -> c -> f b
 f <.> g = fmap f . g
+
+liftAA2 :: (Applicative f1, Applicative f2)
+        => (a -> b -> c) -> f1 (f2 a) -> f1 (f2 b) -> f1 (f2 c)
+liftAA2 = liftA2 . liftA2
+
+newtype TileNum = TileNum Int deriving (Eq, Ord, Show)
+
+instance Bounded TileNum where
+  minBound = TileNum 1
+  maxBound = TileNum 12
+
+instance DownwardEnum TileNum where
+  pred t@(TileNum n)
+    | minBound < t && t <= maxBound = TileNum <$> pred n
+    | otherwise = Nothing
+  precedes (TileNum n) (TileNum m) = minBound <= n && n < m && m <= maxBound
+
+instance UpwardEnum TileNum where
+  succ t@(TileNum n)
+    | minBound <= t && t < maxBound = TileNum <$> succ n
+    | otherwise = Nothing
+  succeeds (TileNum n) (TileNum m) = maxBound >= n && n > m && m >= minBound
+
+instance Enum TileNum where
+  toEnum n
+    | minBound <= tn && tn <= maxBound = Just tn
+    | otherwise = Nothing
+    where tn = TileNum n
+  fromEnum (TileNum n) = Just n
+
+tilenum :: Int -> Maybe TileNum
+tilenum = toEnum
 
 data Color = Black
            | Red
@@ -53,12 +88,69 @@ instance Ord Color where
   compare _        AnyColor = EQ
   compare _        _        = GT
 
-data Tile = NumTile Int Color
+instance DownwardEnum Color where
+  pred Black    = Just Blue
+  pred Red      = Just Black
+  pred Orange   = Just Red
+  pred Blue     = Just Orange
+  pred AnyColor = Just AnyColor
+  precedes AnyColor _ = True
+  precedes _ AnyColor = True
+  precedes x y = x /= y
+
+data Tile = NumTile TileNum Color
           | Joker
           | ColorJoker
           | DoubleJoker
           | MirrorJoker
-          deriving (Eq, Show)
+          deriving Show
+
+instance Eq Tile where
+  MirrorJoker   == MirrorJoker = True
+  Joker         == _           = True
+  ColorJoker    == _           = True
+  DoubleJoker   == _           = True
+  MirrorJoker   == _           = False
+  _             == Joker       = True
+  _             == ColorJoker  = True
+  _             == DoubleJoker = True
+  _             == MirrorJoker = False
+  (NumTile n c) == (NumTile m d) = n   == m && c == d
+
+instance Ord Tile where
+  (<=) (NumTile n c) (NumTile m d) = n <= m && c <= d
+  (<=) _ _ = True
+  (<) Joker _     = True
+  (<) _     Joker = True
+  (<) ColorJoker _     = True
+  (<) _     ColorJoker = True
+  (<) a b = a <= b && a /= b
+
+instance DownwardEnum Tile where
+  pred (NumTile n c) = NumTile n <$> pred c
+  pred joker = Just joker
+  precedes (NumTile n c) (NumTile m d) = n == m && c `precedes` d
+  precedes _ _ = True
+
+instance UpwardEnum Tile where
+  succ (NumTile n c) = flip NumTile c <$> succ n
+  succ joker = Just joker
+  succeeds (NumTile n c) (NumTile m d) = c == d && n `succeeds` m
+  succeeds _ _ = True
+
+(<#)   :: Tile -> Tile -> Bool
+(<#<)  :: Tile -> Tile -> Bool
+(<##<) :: Tile -> Tile -> Bool
+(<#)   = (==) $::  succ                    ~> return ~> id
+(<#<)  = (==) $:: (succ <=< succ)          ~> return ~> id
+(<##<) = (==) $:: (succ <=< succ <=< succ) ~> return ~> id
+
+(<@)   :: Tile -> Tile -> Bool
+(<@<)  :: Tile -> Tile -> Bool
+(<@@<) :: Tile -> Tile -> Bool
+(<@)   = (==) $:: return ~>  pred                    ~> id
+(<@<)  = (==) $:: return ~> (pred <=< pred)          ~> id
+(<@@<) = (==) $:: return ~> (pred <=< pred <=< pred) ~> id
 
 tile :: String -> Maybe Tile
 tile [] = Nothing
@@ -67,23 +159,14 @@ tile s@(c:n)
   | s == "cj" = Just ColorJoker
   | s == "dj" = Just DoubleJoker
   | s == "mj" = Just MirrorJoker
-  | otherwise = liftA2 NumTile (readMaybe n :: Maybe Int) (color c)
-
-instance Ord Tile where
-  (<=) (NumTile n c) (NumTile m d) = (n <= m) && (c <= d)
-  (<=) _ _ = True
-  (<) Joker _     = True
-  (<) _     Joker = True
-  (<) ColorJoker _     = True
-  (<) _     ColorJoker = True
-  (<) a b = a <= b && a /= b
+  | otherwise = liftA2 NumTile (tilenum =<< readMaybe n) (color c)
 
 tileColor (NumTile _ c) = c
 tileColor _ = AnyColor
 
 data Run = NumRun [Tile]
          | ColorRun [Tile]
-         deriving (Eq, Show)
+         deriving Show
 
 allRel :: (b -> [(a, a)]) -> (a -> a -> Bool) -> b -> Bool
 allRel p r = all (uncurry r) . p
@@ -100,9 +183,29 @@ neighbors = ap zip tail
 allNeighbors :: (a -> a -> Bool) -> [a] -> Bool
 allNeighbors = allRel neighbors
 
--- Covers valid DoubleJokers
+acquaintances :: [a] -> [(a, a)]
+acquaintances [_] = []
+acquaintances xs  = ap zip (tail . tail) xs
+
+allAcquaintances :: (a -> a -> Bool) -> [a] -> Bool
+allAcquaintances = allRel acquaintances
+
+triples :: [a] -> [((a, a), a)]
+triples = (liftA2 zip . ap zip) tail (tail . tail)
+
+allTriples :: (a -> a -> Bool) -> [a] -> Bool
+allTriples = liftAA2 (&&) allNeighbors allAcquaintances
+
+duplicateDoubleJokers :: [Tile] -> [Tile]
+duplicateDoubleJokers = intercalate [Joker, Joker] . splitOn [DoubleJoker]
+
 runEqual :: [Tile] -> [Tile] -> Bool
-runEqual = (==) `on` intercalate [Joker, Joker] . splitOn [DoubleJoker]
+runEqual = (==) `on` duplicateDoubleJokers
+
+instance Eq Run where
+  (NumRun x)   == (NumRun y)   = x `runEqual` y
+  (ColorRun x) == (ColorRun y) = x `runEqual` y
+  _ == _ = False
 
 validMirrorJoker :: [Tile] -> Bool
 validMirrorJoker ts = allNeighbors reverseEqual (splitOn [MirrorJoker] ts)
@@ -123,12 +226,20 @@ differentColor = (/=) `on` tileColor
 
 -- TODO: Check valid run length and endpoints for jokers
 -- TODO: Combine all the checks
+-- FIXME: The number run `b1 j b2` tests valid since Ord Tile is insufficient.
 
 all' :: [a -> b -> Bool] -> (a -> b -> Bool)
 all' = (and .:) . sequence <.> sequence
 
-validRun (NumRun ts) = allNeighbors (all' [(<), sameColor]) ts
-validRun (ColorRun ts) = allNeighbors (all' [(<), differentColor]) ts
+validNumRun x y z
+  | otherwise = x `nn` y && y `nn` z && x `na` z
+  where nn = liftAA2 (&&) (<) sameColor
+        na = liftAA2 (&&) (<#<) sameColor
+
+validRun (NumRun ts)   = duplicateDoubleJokers ts & liftA2 (&&)
+                         (allNeighbors     $ liftAA2 (&&) (<)   sameColor)
+                         (allAcquaintances $ liftAA2 (&&) (<#<) sameColor)
+validRun (ColorRun ts) = allTriples (all' [(<), differentColor]) ts
 
 parseTiles :: String -> Maybe [Tile]
 parseTiles = traverse tile . splitOn " "
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,53 @@
 import Test.HUnit
+import Control.Applicative
+import Data.Function
+import Data.Maybe
 import Rummikub
 
+validNumberSucc :: String -> String -> String -> Test
+validNumberSucc s a b = "Valid number run successor (" ++ s ++ ")"
+  ~: (fromMaybe False $ ((liftA2 (<#)) `on` tile) a b) @? c
+  where c = a ++ " <# " ++ b
+
+validNumberSuccTests :: Test
+validNumberSuccTests = TestList $ TestList <$>
+  [ zipWith (validNumberSucc "number-number")
+    ["r1", "b3", "o4", "B2", "*10", "r9",  "*11"]
+    ["r2", "b4", "o5", "B3", "*11", "*10", "b12"]
+  , zipWith (validNumberSucc "repeated jokers")
+    ["j", "cj", "dj", "j", "cj",  "dj", "mj", "mj", "mj"]
+    ["j", "cj", "dj", "mj", "mj", "mj", "j", "cj",  "dj"]
+  , zipWith (validNumberSucc "right jokers")
+    ["b2", "b2", "b2", "o12"]
+    ["j",  "cj", "dj", "mj" ]
+  , zipWith (validNumberSucc "left jokers")
+    ["j",  "cj", "dj", "mj"]
+    ["b2", "b2", "b2", "o1"]]
+
+invalidNumberSucc :: String -> String -> String -> Test
+invalidNumberSucc s a b = "Invalid number run successor (" ++ s ++ ")"
+  ~: not (fromMaybe True $ ((liftA2 (<#)) `on` tile) a b) @? c
+  where c = a ++ " <# " ++ b
+
+invalidNumberSuccTests :: Test
+invalidNumberSuccTests = TestList $ TestList <$>
+  [ zipWith (invalidNumberSucc "number-number")
+    ["r2", "b4", "o5", "B3", "*11", "*10", "b12"]
+    ["r1", "b3", "o4", "B2", "*10", "r9",  "*11"]
+  , zipWith (invalidNumberSucc "center jokers")
+    ["mj"]
+    ["mj"]
+  , zipWith (invalidNumberSucc "right jokers")
+    ["o12", "o12", "o12", "o11"]
+    ["j",   "cj",  "dj",  "dj" ]
+  , zipWith (invalidNumberSucc "left jokers")
+    ["j",  "cj", "dj", "dj" ]
+    ["o1", "o1", "o2", "o2"]]
+
+numberSuccTests :: Test
+numberSuccTests = TestList [validNumberSuccTests, invalidNumberSuccTests]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do (_,s) <- runTestText putTextToShowS numberSuccTests
+          putStr $ s ""
 
